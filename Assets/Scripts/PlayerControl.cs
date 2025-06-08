7ef2e612a6146cff85c8ff9a5541e2129d5bb504
Ronghao <ronghaoluo@gmail.com>
2025-06-07 18:45:26 -0700

    Implemented Movement

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,8 +4,13 @@ public class PlayerControl : MonoBehaviour
 {
     [SerializeField] private float _jumpForce = 1f;
     [SerializeField] private Rigidbody2D _myRigidbody;
+    [SerializeField] private Animator _myAnimator;
+    [SerializeField] private float _horizontalSpeed = 1f;
 
-    public bool canJump;
+    private float _horizontalInput;
+
+    public bool canJump;    // Player is gounded
+    public bool isRunning;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,6 +21,8 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Movements
+        //      Jump
         //transform.position += transform.up * Input.GetAxis("Jump") * Time.deltaTime * _jumpSpeed;
         if (Input.GetKey(KeyCode.Space) && canJump
             )
@@ -24,5 +31,16 @@ public class PlayerControl : MonoBehaviour
             _myRigidbody.AddForce(transform.up * _jumpForce, ForceMode2D.Impulse);
             canJump = false;
         }
+        //      Running
+        _horizontalInput = Input.GetAxis("Horizontal");
+        transform.position += transform.right * _horizontalInput * Time.deltaTime * _horizontalSpeed;
+        if (Input.GetButton("Horizontal")) { isRunning = true; }
+        else { isRunning = false; }
+
+            // Animations
+            //      Jump
+            _myAnimator.SetBool("isGrounded", canJump);
+        //      Running
+        _myAnimator.SetBool("isRunning", isRunning);
     }
 }
